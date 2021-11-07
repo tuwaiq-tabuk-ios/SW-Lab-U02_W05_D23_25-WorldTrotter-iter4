12f9fe680d31eacdf37e2057c835a03ec07ed5e8
mona M <monam@monas-MacBook-Pro.local>
2021-11-07 08:54:05 +0300

    Completed  WorldTrotter-iter4

---
+++ WorldTrotter-iter4/WorldTrotter-MonaMisfer-iter2/Extansions/UIColor+EXt.swift
@@ -0,0 +1,22 @@
+
+
+import UIKit
+
+extension UIColor{
+  static let CMWitebackground = UIColor(red:245.0/255,
+                                        green: 244/255.0,
+                                        blue: 241.0/255.0,
+                                        alpha: 1)
+  
+  static let CMOrange = UIColor (red:225.0/255,
+                                 green: 88.00/255.0,
+                                 blue: 41.0/255.0,
+                                 alpha: 1)
+}
+
+
+
+
+
+
+
---
+++ WorldTrotter-iter4/WorldTrotter-MonaMisfer-iter2/Extansions/UIViewController+Ex.swift
@@ -0,0 +1,22 @@
+
+
+import UIKit
+extension UIViewController {
+  
+  
+  func configureHideKeyboardWhenRootViewTapped(){
+    
+    let tap = UITapGestureRecognizer(
+      target: self,
+      action: #selector(UIViewController.dismissKeyboard)
+    )
+    
+    tap.cancelsTouchesInView = false
+    view.addGestureRecognizer(tap)
+  }
+  
+  
+  @objc func dismissKeyboard(){
+    view.endEditing(true)
+  }
+}
